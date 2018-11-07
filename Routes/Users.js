ad7674db396a37138532778a35c32ceb87f138e0
KieuThang <kieuducthang@gmail.com>
2018-11-07 15:20:40 +0700

    add/get room

--- Routes/Users.js
+++ Routes/Users.js
@@ -3,6 +3,7 @@ var users = express.Router();
 var database = require('../Database/database');
 var cors = require('cors')
 var jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
 var token;
 
 users.use(cors());
@@ -40,32 +41,42 @@ users.post('/register', function (req, res) {
                         appData.message = "This user already exist!";
                         res.status(200).json(appData);
                     } else {
-                        connection.query('INSERT INTO users SET ?', userData, function (err, rows, fields) {
-                            if (!err) {
-                                connection.query("SELECT * FROM users WHERE email = ?", [userData.email], function (err, rows, fields) {
-                                    if (err) {
-                                        appData.code = 1;
-                                        appData.message = "Error Occurred!";
-                                        res.status(400).json(appData);
-                                    } else {
-                                        appData.code = 0;
-                                        appData.message = "User Registered successfully!";
-                                        var tokenData = {};
-                                        token = jwt.sign(rows[0], process.env.SECRET_KEY, {
-                                            expiresIn: 5000
+                        bcrypt.hash(userData.password, 10, function (err, hash) {
+                            // Store hash in database
+                            if (err) {
+                                appData.code = 1;
+                                appData.message = "Error Occurred!";
+                                res.status(400).json(appData);
+                            } else {
+                                userData.password = hash
+                                connection.query('INSERT INTO users SET ?', userData, function (err, rows, fields) {
+                                    if (!err) {
+                                        connection.query("SELECT * FROM users WHERE email = ?", [userData.email], function (err, rows, fields) {
+                                            if (err) {
+                                                appData.code = 1;
+                                                appData.message = "Error Occurred!";
+                                                res.status(400).json(appData);
+                                            } else {
+                                                appData.code = 0;
+                                                appData.message = "User Registered successfully!";
+                                                var tokenData = {};
+                                                token = jwt.sign(rows[0], process.env.SECRET_KEY, {
+                                                    expiresIn: 86400
+                                                });
+                                                tokenData.token = token;
+                                                tokenData.isActive = (rows[0].isActive == 1);
+                                                tokenData.isDeleted = (rows[0].isDeleted == 1);
+                                                appData.data = tokenData;
+                                                res.status(200).json(appData);
+                                            }
                                         });
-                                        tokenData.token = token;
-                                        tokenData.isActive = (rows[0].isActive == 1);
-                                        tokenData.isDeleted = (rows[0].isDeleted == 1);
-                                        appData.data = tokenData;
-                                        res.status(200).json(appData);
+                                    } else {
+                                        console.log("error:" + err.sqlMessage)
+                                        appData.code = 1;
+                                        appData.message = "Error Occured! ";
+                                        res.status(400).json(appData);
                                     }
                                 });
-                            } else {
-                                console.log("error:" + err.sqlMessage)
-                                appData.code = 1;
-                                appData.message = "Error Occured! ";
-                                res.status(400).json(appData);
                             }
                         });
                     }
@@ -97,25 +108,27 @@ users.post('/login', function (req, res) {
                     res.status(400).json(appData);
                 } else {
                     if (rows.length > 0) {
-                        if (rows[0].password == userData.password) {
-                            token = jwt.sign(rows[0], process.env.SECRET_KEY, {
-                                expiresIn: 5000
-                            });
-                            appData.code = 0;
-                            appData.message = "Success";
-                            var tokenData = {};
-                            tokenData.token = token;
-                            tokenData.isActive = (rows[0].isActive == 1);
-                            tokenData.isDeleted = (rows[0].isDeleted == 1);
-                            appData.data = tokenData;
+                        console.log("password req:"+userData.password+", password res:"+rows[0].password)
+                        bcrypt.compare(userData.password, rows[0].password, function (err, match) {
+                            if (match) {
+                                token = jwt.sign(rows[0], process.env.SECRET_KEY, {
+                                    expiresIn: 86400
+                                });
+                                appData.code = 0;
+                                appData.message = "Success";
+                                var tokenData = {};
+                                tokenData.token = token;
+                                tokenData.isActive = (rows[0].isActive == 1);
+                                tokenData.isDeleted = (rows[0].isDeleted == 1);
+                                appData.data = tokenData;
 
-                            res.status(200).json(appData);
-                        }
-                        else {
-                            appData.code = 1;
-                            appData.message = "Email or password do not match";
-                            res.status(200).json(appData);
-                        }
+                                res.status(200).json(appData);
+                            } else {
+                                appData.code = 1;
+                                appData.message = "Email or password do not match";
+                                res.status(200).json(appData);
+                            }
+                        });
                     } else {
                         appData.code = 1;
                         appData.message = "Email does not exists!";
@@ -130,9 +143,10 @@ users.post('/login', function (req, res) {
 
 users.use(function (req, res, next) {
     var token = req.body.token || req.headers['token'];
+
     var appData = {};
     if (token) {
-        jwt.verify(token, process.env.SECRET_KEY, function (err) {
+        jwt.verify(token, process.env.SECRET_KEY, function (err, mutatePayload) {
             if (err) {
                 appData.code = 1;
                 appData.message = "Token is invalid";
@@ -149,7 +163,6 @@ users.use(function (req, res, next) {
 });
 
 users.get('/getUsers', function (req, res) {
-    var token = req.body.token || req.header['token'];
     var appData = {};
 
     database.connection.getConnection(function (err, connection) {
@@ -167,6 +180,47 @@ users.get('/getUsers', function (req, res) {
                     appData.code = 0;
                     appData.data = rows;
                     res.status(200).json(appData);
+                }
+            });
+            connection.release();
+        }
+    });
+});
+
+users.get('/getProfile', function (req, res) {
+    var token = req.body.token || req.headers['token'];
+    var decoded = jwt.decode(token, process.env.SECRET_KEY);
+    console.log("token:" + token + ",decode:" + decoded.va)
+    var userData = {};
+    var appData = {};
+   
+    database.connection.getConnection(function (err, connection) {
+        if (err) {
+            appData.code = 1;
+            appData.message = "Internal Server Error!";
+            res.status(500).json(appData);
+        } else {
+            connection.query('SELECT * FROM users where id = ?', decoded.id, function (err, rows, fields) {
+                if (err) {
+                    appData.code = 1;
+                    appData.message = "No data found";
+                    res.status(200).json(appData);
+                } else {
+                    if (rows.length > 0) {
+                        appData.code = 0;
+                        userData.first_name = rows[0].first_name
+                        userData.last_name = rows[0].last_name
+                        userData.email = rows[0].email
+                        userData.id = rows[0].id
+                        userData.isActive = (rows[0].isActive == 1)
+                        userData.isDeleted = (rows[0].isDeleted == 1)
+                        appData.data = userData
+                        res.status(200).json(appData);
+                    } else {
+                        appData.code = 1;
+                        appData.message = "No data found";
+                        res.status(200).json(appData);
+                    }
                 }
             });
             connection.release();
--- server.js
+++ server.js
@@ -11,11 +11,12 @@ app.use(bodyParser.urlencoded({
     extended: false
 }));
 
-var Users = require('./Routes/Users');
-
 app.use(express.static(path.join(__dirname, '/public/')));
+var Users = require('./Routes/Users');
+app.use('/users', Users);
 
-app.use('/users',Users);
+var Rooms = require('./Routes/Rooms')
+app.use('/rooms', Rooms)
 
 app.get('/', function (req, res) {
     var appData = {};
@@ -23,11 +24,11 @@ app.get('/', function (req, res) {
     res.status(200).json(appData);
 })
 
-app.get('/login',function(req, res){
-    res.sendFile( __dirname + "/public/login/" + "login.html" );  
+app.get('/login', function (req, res) {
+    res.sendFile(__dirname + "/public/login/" + "login.html");
 });
 
 
-app.listen(port,function(){
-    console.log("Server is running on port: "+port);
+app.listen(port, function () {
+    console.log("Server is running on port: " + port);
 });
