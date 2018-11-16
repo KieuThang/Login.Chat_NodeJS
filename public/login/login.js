2ad7296f6aceea6afa18a8b696f4d5d6024c968d
KieuThang <kieuducthang@gmail.com>
2018-11-16 17:47:43 +0700

    update login/sign up on web portal

--- public/login/login.js
+++ public/login/login.js
@@ -1,47 +1,39 @@
-var attempt = 3; // Variable to count number of attempts.
-// Below function Executes on click of login button.
-function onLogin() {
-    // alert("Login successfully");
-    console.log("Test test");
-    var email = document.getElementById("email").value;
-    var password = document.getElementById("password").value;
-    if (email == null || email == "") {
-        alert("Email cannot empty:" + email);
-        return true;
-    }
-    if (password == null || password == "") {
-        alert("Password is not valid");
-        return true;
-    }
-    const userData = {
-        "email": email,
-        "password": password
-    }
-    const Http = new XMLHttpRequest();
-    const url = 'https://jsonplaceholder.typicode.com/posts';
-    Http.open("GET", url);
-    Http.send();
-    Http.onreadystatechange = (e) => {
-        console.log(Http.responseText)
-    }
-    // if (username == "Formget" && password == "formget#123") {
-    //     alert("Login successfully");
-    //     window.location = "/signup/signup.html"; // Redirecting to other page.
-    //     return false;
-    // }
-    // else {
-    //     attempt--;// Decrementing by one.
-    //     alert("You have left " + attempt + " attempt;");
-    //     // Disabling fields after 3 attempts.
-    //     if (attempt == 0) {
-    //         document.getElementsByName("username").disabled = true;
-    //         document.getElementsByName("password").disabled = true;
-    //         document.getElementById("submit").disabled = true;
-    //         return false;
-    //     }
-    // }
-    return false;
-}
+$(function () {
+    $('.login-form').on('submit', function (event) {
+        event.preventDefault();
+        var email = $("#email").val();
+        var password = $("#password").val();
+
+        var isOk = true;
+
+        if (email == null || email == "") {
+            alert("Email cannot empty:" + email);
+            isOk = false;
+        }
+        if (password == null || password == "") {
+            alert("Password is not valid");
+            isOk = false;
+        }
+        if (!isOk) return;
+
+        const userData = {
+            "email": email,
+            "password": password
+        }
+
+        $.post('/users/login', { email: email, password: password }, function (data) {
+            if(data.code != 0){
+                alert(data.message);
+                return;
+            }
+
+            localStorage.accessToken = JSON.stringify(data);
+            window.event.returnValue = false;
+            console.log(localStorage.accessToken);
+            window.location.href = "/main/index.html";
+        });
+    });
+});
 
 function onRedirectLoginPage() {
     window.event.returnValue = false;
